c39e66132acfb56d8bbe40d7d4b0c56e5a5331cd
ksw <ksw@ehcosoft.com>
2024-09-06 14:59:34 +0900

    20240906 추가 컨텐츠 및 기존요소 수정

--- resources/assets/js/kw-common.js
+++ resources/assets/js/kw-common.js
@@ -43,4 +43,31 @@ $(document).ready(function () {
         $('.overlay').removeClass('active');
     });
 
+    /***
+      * 모달 이벤트
+   ****/
+
+    function removeDefaultEvent(e) {
+        e.preventDefault();
+    }
+
+    // 모달 열기 버튼 클릭 이벤트
+    $(".open-modal").click(function () {
+        var modalId = $(this).data("modal-id");
+        $("#" + modalId).addClass("active");
+        $("body").css("overflow", "hidden");
+        // window.addEventListener("wheel", removeDefaultEvent, { passive: false });
+    });
+
+    // 모달 닫기 버튼 및 모달 바깥 영역 클릭 이벤트
+    $(".btn-modal-close, .kw-modal-wrap").click(function () {
+        $(".kw-modal-wrap").removeClass("active");
+        $("body").css("overflow", "auto");
+        // window.removeEventListener("wheel", removeDefaultEvent);
+    });
+
+    // 모달 내부 클릭 시 닫기 방지
+    $(".kw-modal-content").click(function (e) {
+        e.stopPropagation();
+    });
 });
